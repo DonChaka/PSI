14e39f481a29a9ce6a085a4f51f0990d5b7aee2c
DonChaka <DonChaka98@gmail.com>
2022-05-12 12:26:26 +0200

    czolg, nie zrobione

---
+++ PAG/GAZamowienia.py
@@ -0,0 +1,7 @@
+import struct
+
+def float_to_bin(num):
+    return bin(struct.unpack('!I', struct.pack('!f', num))[0])[2:].zfill(32)
+
+def bin_to_float(binary):
+    return struct.unpack('!f',struct.pack('!I', int(binary, 2)))[0]
